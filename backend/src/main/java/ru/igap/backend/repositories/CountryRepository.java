c5048450254845ded8a6bcb6f5c11e774c4ba932
FizMatXimik <cool.gaplikov2014@yandex.ru>
2022-04-14 20:34:10 +0300

    laba5_final

--- backend/src/main/java/ru/igap/backend/repositories/CountryRepository.java
+++ backend/src/main/java/ru/igap/backend/repositories/CountryRepository.java
@@ -1,4 +1,12 @@
 package ru.igap.backend.repositories;
 
-public class CountryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.igap.backend.models.Country;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long>
+{
+
 }
+
